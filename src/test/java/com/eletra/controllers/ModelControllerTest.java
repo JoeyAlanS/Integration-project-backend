6c1d03f2b0b8c21c0ecbe6cc21c0d93ffde97b9d
Joey Alan <joeyalan50@gmail.com>
2024-06-07 12:56:25 -0300

    ###initial commit

---
+++ src/test/java/com/eletra/controllers/ModelControllerTest.java
@@ -0,0 +1,29 @@
+package com.eletra.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("test")
+class ModelControllerTest {
+
+    @Test
+    void getModelEntityList() {
+    }
+
+    @Test
+    void getModelEntityByCategory() {
+    }
+
+    @Test
+    void postModelEntity() {
+    }
+
+    @Test
+    void deleteModelEntity() {
+    }
+
+    @Test
+    void updateModelEntity() {
+    }
+}
